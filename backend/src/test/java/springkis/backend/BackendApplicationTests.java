5eda769de2c42ae4957e71710eea7063c7e30fa8
hl3470 <hl3470@naver.com>
2024-09-16 14:45:18 +0900

    init commit

---
+++ backend/src/test/java/springkis/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package springkis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
